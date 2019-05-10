f302a1496df8af1a439d5daba6a00fff81b187da
Tom Biju <tombiju95@yahoo.com>
2019-05-10 15:10:14 -0500

    tweak styling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail, Message
-import configparser
+import configparser, requests
 
 app = Flask(__name__)
 config = configparser.ConfigParser()
@@ -21,7 +21,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/send_email', methods=['POST'])
-def send_email():
+# def send_email():
     # first_name =  request.form.get('first_name')
     # last_name = request.form.get('last_name')
     # email = request.form.get('email')
@@ -34,6 +34,15 @@ def send_email():
     #                     recipients=["<***REMOVED***>"], # replace with your email for testing
     #                     body=text)
     #         mail.send(msg)
+    # return redirect("/")
+def send_email():
+    requests.post(
+        "https://api.mailgun.net/v3/sandboxa2de269106534839a8c7be8d4137316d.mailgun.org/messages",
+        auth=("api", "ab3950059524177b51c95f68b5ce1c78-7bce17e5-ccd1a7d5"),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxa2de269106534839a8c7be8d4137316d.mailgun.org>",
+              "to": "Tom Biju <***REMOVED***>",
+              "subject": "New Inquiry",
+              "text": "Congratulations Tom Biju, you just sent an email with Mailgun!  You are truly awesome!"})
     return redirect("/")
 
 if __name__ == '__main__':
