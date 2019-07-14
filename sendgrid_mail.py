97a4a80c6f8819af1dfc184165095e6b1a62ef83
Tom Biju <tombiju95@yahoo.com>
2019-07-13 20:48:40 -0500

    update emails

--- sendgrid_mail.py
+++ sendgrid_mail.py
@@ -4,15 +4,17 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 api_key = "***REMOVED***"
+emails = [***REMOVED***]
 
 
 def dispatch_mailman(first_name, last_name, email, comments):
     message = Mail(
         from_email='***REMOVED***',
-        to_emails='***REMOVED***',
+        to_emails=emails
         subject='Sending with Twilio SendGrid is Fun',
-        html_content="""Sender: <strong>{} {}</strong> \n
-            Email: <strong>{}</strong> \n Message: <strong>{}</strong> \n"""
+        html_content="""Sender: <strong>{} {}</strong> <br/>
+            Email: <strong>{}</strong> <br/>
+            Message: <strong>{}</strong> <br/> """
                 .format(first_name, last_name, email, comments))
     try:
         sg = SendGridAPIClient(api_key)
