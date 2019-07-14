d4847a504b22cbe47cac63937f8a44d801986837
Tom Biju <tombiju95@yahoo.com>
2019-07-13 20:19:08 -0500

    integrate sendgrid mail

--- app.py
+++ app.py
@@ -1,49 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_mail import Mail, Message
-import configparser, requests
+import urllib.request as urllib2
+from flask import Flask, render_template, request, redirect
+from sendgrid_mail import dispatch_mailman
+# email.py renamed to sendgrid_mail.py due to library conflict
 
 app = Flask(__name__)
-config = configparser.ConfigParser()
-config.read('config.ini')
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,  # 465 for SSL, 587 for TLS
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": config['email']['username'],
-    "MAIL_PASSWORD": config['email']['password']
-}
-app.config.update(mail_settings)
-mail = Mail(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/send_email', methods=['POST'])
-# def send_email():
-    # first_name =  request.form.get('first_name')
-    # last_name = request.form.get('last_name')
-    # email = request.form.get('email')
-    # comments = request.form.get('comments')
-    # text = "Sender: {} {} \n Email: {} \n Message: {} \n".format(first_name, last_name, email, comments)
-    # if None not in [first_name, last_name, email, comments]:
-    #     with app.app_context():
-    #         msg = Message(subject="KV Beats Website Message",
-    #                     sender=app.config.get("MAIL_USERNAME"),
-    #                     recipients=["<***REMOVED***>"], # replace with your email for testing
-    #                     body=text)
-    #         mail.send(msg)
-    # return redirect("/")
 def send_email():
-    requests.post(
-        "https://api.mailgun.net/v3/sandboxa2de269106534839a8c7be8d4137316d.mailgun.org/messages",
-        auth=("api", "ab3950059524177b51c95f68b5ce1c78-7bce17e5-ccd1a7d5"),
-        data={"from": "Mailgun Sandbox <postmaster@sandboxa2de269106534839a8c7be8d4137316d.mailgun.org>",
-              "to": "Tom Biju <***REMOVED***>",
-              "subject": "New Inquiry",
-              "text": "Congratulations Tom Biju, you just sent an email with Mailgun!  You are truly awesome!"})
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+    comments = request.form.get('comments')
+    if None not in [first_name, last_name, email, comments]:
+        dispatch_mailman(first_name, last_name, email, comments)
     return redirect("/")
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)  # True only for development
---
+++ sendgrid_mail.py
@@ -0,0 +1,26 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+api_key = """
+***REMOVED***
+"""
+
+
+def dispatch_mailman(first_name, last_name, email, comments):
+    message = Mail(
+        from_email='***REMOVED***',
+        to_emails='***REMOVED***',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content="""Sender: <strong>{} {}</strong> \n
+            Email: <strong>{}</strong> \n Message: <strong>{}</strong> \n"""
+                .format(first_name, last_name, email, comments))
+    try:
+        sg = SendGridAPIClient(api_key)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
