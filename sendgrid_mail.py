d55ea6183300afbaef9109576d0c3224eb45f418
tombiju <tombiju95@yahoo.com>
2023-03-15 01:01:53 -0500

    integrate more config vars

--- sendgrid_mail.py
+++ sendgrid_mail.py
@@ -5,18 +5,18 @@ from sendgrid.helpers.mail import Mail
 import os
 
 api_key = os.environ['SENDGRID_API_KEY']
-emails = [***REMOVED***]
+emails = os.environ['RECIPIENT_EMAILS'].split(",")
 
 
-def dispatch_mailman(first_name, last_name, email, comments):
+def dispatch_mailman(first_name, last_name, email, message_body):
     message = Mail(
-        from_email=os.environ['FROM_EMAIL'],
+        from_email=os.environ['SENDER_EMAIL'],
         to_emails=emails,
         subject='KV Beats Website Message!',
         html_content="""Sender: <strong>{} {}</strong> <br/>
             Email: <strong>{}</strong> <br/>
             Message: <strong>{}</strong> <br/> """
-                .format(first_name, last_name, email, comments))
+                .format(first_name, last_name, email, message_body))
     try:
         sg = SendGridAPIClient(api_key)
         response = sg.send(message)
